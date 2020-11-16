93a727ac59c8fb0edb572a13c25922f97263be0f
itsmeskhere <sumankalyangiri786@gmail.com>
2020-11-16 21:20:25 +0530

    frequencyList function added

--- C++/try.cpp
+++ C++/try.cpp
@@ -112,18 +112,74 @@ class SLL {
 			}
 			if(temp == NULL){
 				cout<<"\nMin Node: "<<min;
+			}	
+		}
+		
+		void frequencyList(){
+			cout<<"\nFrequency Table";
+			int max = -999999999;
+			int min = 999999999;
+			node *temp = new node();
+			temp = head;
+			while(temp != NULL){
+				if(min > temp->data){
+					min = temp->data;
+				}
+				if(max < temp->data){
+					max = temp->data;
+				}
+				temp = temp->next;
+			}
+			
+			min*=-1;
+			int pos[max+1] = {0};
+			int neg[min+1] = {0};
+			if(max == -999999999){
+				max = 0;
+			}
+			if(min == -999999999){
+				min = 0;
+			}
+			
+			temp = head;
+			while(temp != NULL){
+				if(temp->data >= 0){
+					pos[temp->data]++;
+				}
+				else if(temp->data < 0){
+					int x;
+					x = temp->data;
+					x*=-1;
+					neg[x]++;
+				}
+				temp = temp->next;
+			}
+			for(int i = min; i > 0; i--){
+				if(neg[i] != 0){
+					cout<<"\n-"<<i<<"==>"<<neg[i];
+				}
+			}
+			for(int i = 0; i < max+1; i++){
+				if(pos[i] != 0){
+					cout<<"\n"<<i<<"==>"<<pos[i];
+				}
 			}
 		}
 };
 
 int main(){
 	SLL l;
-	l.insertNode(1,0);
-	l.insertNode(0,1);
-	l.insertNode(2,2);
-	l.insertNode(3,-1);
-	l.insertNode(4,-2);
+	l.insertNode(-1,0);
+	l.insertNode(-10,1);
+	l.insertNode(0,2);
+	l.insertNode(-1,3);
+	l.insertNode(1,4);
+	l.insertNode(1,5);
+	l.insertNode(0,6);
+	l.insertNode(2,7);
 	l.display();
 	l.maxNode();
 	l.minNode();
+	l.frequencyList();
+	l.display();
 }
